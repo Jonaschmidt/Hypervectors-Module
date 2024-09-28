8936f52d924cfc172a1658f5837462815d54ee65
Jonaschmidt <jonaschmidt.en@gmail.com>
2024-09-27 23:18:17 -0500

    addition: Sobol sequence hv generation implemented (still experimental)

--- hypervectors.py
+++ hypervectors.py
@@ -1,9 +1,28 @@
 # TODO: docstrings, especially of the static methods
+# TODO: determine class/function structure
 # TODO: support for binary and polar Hypervectors?
 
+from typing import Literal, Optional, Tuple
 import tensorflow as tf
-from typing import Optional, Tuple
+import math
+import numpy as np
+from scipy.stats import qmc
 
+# globals for Sobol generation...
+sobol_dim = 0
+sobol_indexer = 0
+sobol_seqs = None
+# TODO: give function better name
+def _comp_each_to_val(x, val):
+    return 1 - (2 * (x > val))
+_comp_each_to_val = np.vectorize(_comp_each_to_val)
+# ...
+
+# TODO: be more clear about value everywhere, should it just always be a ratio?
+def gen_tensor_sobol(value, size, value_range: Optional[Tuple[int, int]] = None):
+    pass
+
+# TODO: full implementation of Sobol sequences
 # TODO: fix or remove or update the value_range and value attribute:
 #           (consider that these can/should be "overridden" when generating LHVs)
 # TODO: docstrings
@@ -19,7 +38,12 @@ class Hypervector:
         tensor (tensorflow.python.framework.ops.EagerTensor): The actual Hypervector, represented as a tensor
     '''
 
-    def __init__(self, size: Optional[int]=256, value: Optional[int]=None, value_range: Optional[Tuple[int, int]]=None, tensor:Optional[tf.Tensor]=None):
+    def __init__(self, 
+                 size: Optional[int]=256, 
+                 value: Optional[int]=None, 
+                 value_range: Optional[Tuple[int, int]]=None, 
+                 tensor: Optional[tf.Tensor]=None,
+                 random_method: Literal["tf_random", "Sobol"]="tf_random"):
         '''
         Initializes a Hypervector with the given size, value, and value range.
 
@@ -27,6 +51,10 @@ class Hypervector:
             size (int): The size of the Hypervector, must be positive.
             value (Optional[int]): The value represented by the Hypervector. Default is None, which sets the value to half the size.
             value_range (Optional[Tuple[int, int]]): The range of possible values the Hypervector can represent. Default is None, which sets the range to (0, size).
+            tensor (Optional[tf.Tensor]): Can be used to initialize the tensor of a Hypervector with finer control.
+            random_method (Literal["built-in", "Sobol"]): Determines method of Hypervector generation:
+                - "tf_random" specifies utilization of the built-in tensorflow random method
+                - "Sobol" specifies utilization of Sobol sequences to generate Hypervectors, as described in the literature [TODO: citation]
         '''
 
         if size <= 0:
@@ -43,11 +71,15 @@ class Hypervector:
             self.value = self._generate_value()
             self.size = self.tensor.shape[0]
         else:
-            self.tensor = self._generate_tensor(value_range)
+            self.tensor = self._generate_tensor(value=self.value, value_range=value_range, random_method=random_method)
 
-    def _generate_tensor(self, value_range: Optional[Tuple[int, int]] = None) -> tf.Tensor:
+    def _generate_tensor(self, value, value_range: Optional[Tuple[int, int]] = None, random_method: Literal["tf_random", "Sobol"]="tf_random") -> tf.Tensor:
         '''
         Generates the tensor representation of the Hypervector.
+        Two generation methods are supported
+        (TODO: describe these methods in detail:)
+        - tf_random
+        - Sobol
 
         Args:
             value_range (Optional[Tuple[int, int]]): The range of possible values the Hypervector can represent. Default is None, which sets the range to (0, size).
@@ -55,21 +87,49 @@ class Hypervector:
         Returns:
             tf.Tensor: The generated tensor representing the Hypervector.
         '''
-        if self.value is None:
-            self.value = self.size / 2
+        if value is None:
+            value = self.size / 2
 
         if value_range is None:
             value_range = (0, self.size)
 
         min_value, max_value = value_range
-        max_value += 1
 
-        num_ones = int(self.size * (self.value + min_value) / (max_value - 1))
-        corr_vec = tf.constant([-1] * self.size)
-        indices = tf.random.shuffle(tf.range(self.size))[:num_ones]
-        updates = tf.ones(num_ones, dtype=tf.int32)
+        # tf_random GENERATION
+        if random_method == "tf_random":
+            num_ones = int(self.size * (value + min_value) / max_value)
+            corr_vec = tf.constant([-1] * self.size)
+            indices = tf.random.shuffle(tf.range(self.size))[:num_ones]
+            updates = tf.ones(num_ones, dtype=tf.int32)
 
-        return tf.tensor_scatter_nd_update(corr_vec, tf.expand_dims(indices, 1), updates)
+            tensor = tf.tensor_scatter_nd_update(corr_vec, tf.expand_dims(indices, 1), updates)
+        
+        # Sobol GENERATION
+        elif random_method == "Sobol":
+            global sobol_dim, sobol_indexer, sobol_seqs
+    
+            if sobol_seqs is None or sobol_dim < self.size:
+                m = math.ceil(math.log(self.size, 2))
+                sobol_dim = 2**m
+
+                sobol_engine = qmc.Sobol(d=21_201, scramble=False)  # the maximum number of sequences that can be "generated" by this implementation is 21_201
+                                                                    # TODO: uncap this maximum number of sequences
+
+                sobol_samples = sobol_engine.random_base2(m=m)
+                sobol_seqs = np.transpose(sobol_samples)
+            
+            s_val = ((value - min_value) / (max_value - min_value))
+            sobol_indexer += 1
+
+            tensor = tf.convert_to_tensor(_comp_each_to_val(sobol_seqs[sobol_indexer - 1], s_val)[:self.size])
+
+        # TODO: handle this case better
+        else:
+            print("WARNING PLACEHOLDER")
+
+            tensor = None
+
+        return tensor
     
     def _generate_value(self) -> int:
         '''
